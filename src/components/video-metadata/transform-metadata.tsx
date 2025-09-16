d51e6878fa7943d22220beb1dc4370f3b6e79c63
cuginoAle <alessio.carnevale@mnscorp.net>
2025-09-16 18:53:30 +0200

    feat: enhance metadata transformation with date formatting and display

--- src/components/video-metadata/transform-metadata.tsx
+++ src/components/video-metadata/transform-metadata.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
 import { GoProModel } from 'components/gopro-model';
+import { Text } from '@radix-ui/themes';
+const dateTimeFormat = new Intl.DateTimeFormat(undefined, {
+  dateStyle: 'medium',
+  timeStyle: 'medium',
+});
 
 const transformMetadata = (
   metadata: Record<string, string | number | null | boolean> | null | undefined,
@@ -10,6 +15,31 @@ const transformMetadata = (
   };
 
   transformed['model'] = <GoProModel model={metadata['model'] as string} />;
+  const createDate = dateTimeFormat
+    .format(new Date(metadata['create_date'] as number))
+    .split(', ');
+  transformed['create_date'] = (
+    <span>
+      <Text color="amber" weight="bold">
+        {createDate[0]}{' '}
+      </Text>
+      {createDate[1]}
+    </span>
+  );
+
+  const modifyDate = dateTimeFormat
+    .format(new Date(metadata['modify_date'] as number))
+    .split(', ');
+
+  transformed['modify_date'] = (
+    <span>
+      <Text color="amber" weight="bold">
+        {modifyDate[0]}{' '}
+      </Text>
+      {modifyDate[1]}
+    </span>
+  );
+
   return transformed;
 };
 
